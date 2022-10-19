42ce8ed486aecccc289cf19595d47cf641aff50f
Paulo Serrano <pvsserrano@gmail.com>
2022-10-19 12:11:56 -0300

    enums

--- src/enums/fields.js
+++ src/enums/fields.js
@@ -1,5 +1,42 @@
 const FIELDS = Object.freeze({
+    USERS: {
+        ID: "users.id",
+        EMAIL: "users.email",
+        PASSWORD: "users.password",
+        NAME: "users.name",
+        PICTURE_URL: 'users."pictureUrl"'
+    },
 
+    SESSIONS: {
+        ID: "sessions.id",
+        TOKEN: "sessions.token",
+        ACTIVE: "sessions.active"
+    },
+    
+    POSTS: {
+        ID: "posts.id",
+        USER_ID: 'posts."userId"',
+        LINK: "posts.link",
+        DESCRIPTION: "posts.description"
+        
+    },
+    
+    HASHTAGS: {
+        ID: "hashtags.id",
+        NAME: "hashtags.name"
+    },
+    
+    POSTS_HASHTAGS: {
+        ID: "posts_hashtags.id",
+        POST_ID: 'posts_hashtags."postId"',
+        HASHTAG_ID: 'posts_hashtags."hashtagId"'
+    },
+    
+    LIKES: {
+        ID: "likes.id",
+        USER_ID: 'likes."userId"',
+        POST_ID: 'likes."postId"',
+    }
 })
 
 export { FIELDS }
--- src/enums/tables.js
+++ src/enums/tables.js
@@ -1,5 +1,10 @@
 const TABLES = Object.freeze({
-
+    USERS: "users",
+    SESSIONS: "sessions",
+    POSTS: "posts",
+    HASHTAGS: "hashtags",
+    POSTS_HASHTAGS: "posts_hashtags",
+    LIKES: "likes"
 })
 
 export { TABLES }
